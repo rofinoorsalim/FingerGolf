3beba384c30ab2ab3e5f9e327ef5e9ee7c48f262
rofinoorsalim <58476731+rofinoorsalim@users.noreply.github.com>
2022-10-31 00:29:27 +0700

    Adding Level
    Add 5 level selection & fixing bugs

---
+++ Assets/Script/Aim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField] float speed;
+    float timer;
+
+    private void Update()
+    {
+        if (timer > 1)
+        {
+            timer += Time.deltaTime * speed;
+        }
+
+        var z = Mathf.Lerp(0, 360, timer);
+        this.transform.localRotation = Quaternion.Euler(0, 0, z);
+    }
+}
